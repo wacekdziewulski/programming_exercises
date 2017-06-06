7b89d0f377f1fa9843e51b5da449dd2b4d303fed
Waclaw.Dziewulski <wacekdziewulski@gmail.com>
2017-06-06 16:43:19 +0200

    Added an excercise for implementing a binary tree in C.

---
+++ trees/c/trees.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+    void* left;
+    void* right;
+    int value;
+} Node;
+
+void deleteNode(Node* node) {
+    free(node);
+    node = NULL;
+}
+
+void printNodeValue(Node* node) {
+    printf("%d", node->value);
+}
+
+Node* allocateNode(int value) {
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->left = NULL;
+    node->right = NULL;
+    node->value = value;
+    return node;
+}
+
+/* @return -> indicates if the addition of the node was successful */
+bool addNode(Node* root, int value) {
+    Node** currentNode = &root;
+    Node* newNode = allocateNode(value);
+    if (!newNode) {
+        return false;
+    }
+
+    while (1) {
+        if (!*currentNode) {
+            *currentNode = newNode;
+            break;
+        }
+        else if (*currentNode->value < value) {
+            if (!*currentNode->left) {
+                *currentNode->left = newNode;
+            }
+            *currentNode = *currentNode->left;
+        }
+        else {// (root->value >= value) {
+            if (!*currentNode->right) {
+                *currentNode->right = newNode;
+            }
+            *currentNode = *currentNode->right;
+        }
+    }
+
+    return true;
+}
+
+void dfs(Node* node, void (action)(Node*)) {
+    if (!node) {
+        return;
+    }
+
+    dfs(node->left, action);
+
+    if (action) {
+        (*action)(node);
+    }
+
+    dfs(node->right, action);
+}
+
+int main (char** argv, int argc) {
+    Node* tree;
+    addNode(tree, 3);
+    addNode(tree, 1);
+    addNode(tree, 2);
+    addNode(tree, 5);
+    addNode(tree, 4);
+    addNode(tree, 6);
+    dfs(tree, &printNodeValue);
+
+    return 0;
+}
